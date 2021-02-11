85900bc6e64455b472162e3ace18290fb80e6fcb
Pablo Garcia Andres <p.garciaandres@douglas.de>
2021-02-11 12:55:54 +0100

    SELECT-4691 - Books, first approach

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -35,7 +35,11 @@ const BookListContainer = styled.div`
             h1 {
                 margin-bottom: 3px;
             }
-            p.unknown {
+            h4 {
+                margin-top: 10px;
+                margin-bottom: 3px;
+            }
+            .italic {
                 font-style: italic;
             }
         }
@@ -54,69 +58,79 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+let bookCache = {};
+
 const BookList = props => {
     const classes = useStyles();
     const [expanded, setExpanded] = useState(null);
-    const [bookDetails, setBookDetails] = useState(null);
-    const [loadingImg, setLoadingImg] = useState(true);
 
-    const handleChange = (panel, book) => async(event, isExpanded) => {
-        setBookDetails(null);
-        setExpanded(null);
-        if (isExpanded) {
+    const handleChange = (panel, book, index) => async(event, isExpanded) => {
+        if (bookCache[index]) {
+            setExpanded(isExpanded && panel);
+        }
+        if (isExpanded && !bookCache[index]) {
             const coverEditionKey = book.cover_edition_key ?? '';
-            // si !coverEditionKey, ya sé que la siguiente llamada devolverá un error
-            searchService.getBookByEditionKey(coverEditionKey).then(editionResponse => {
-                const bookCoverEK = editionResponse.data;
-                searchService.getBookByIsbn(bookCoverEK.isbn_10).then(isbnResponse => {
-                    const bookISBN = isbnResponse.data;
-                    const bookCover = bookISBN[`ISBN:${bookCoverEK.isbn_10}`]?.cover?.medium ? bookISBN[`ISBN:${bookCoverEK.isbn_10}`]?.cover?.medium : noCoverImg;
-                    setBookDetails({
-                        title: book.title,
-                        author: book.author_name ? book.author_name[0] : 'Unknown author',
-                        cover: bookCover,
-                        url: book.edition_key.map((i) => `https://openlibrary.org/books/${i}`),
+            if (coverEditionKey) {
+                searchService.getBookByEditionKey(coverEditionKey).then(editionResponse => {
+                    const bookCoverEK = editionResponse.data;
+                    searchService.getBookByIsbn(bookCoverEK.isbn_10).then(isbnResponse => {
+                        const bookISBN = isbnResponse.data;
+                        const bookCover = bookISBN[`ISBN:${bookCoverEK.isbn_10}`]?.cover?.medium ? bookISBN[`ISBN:${bookCoverEK.isbn_10}`]?.cover?.medium : noCoverImg;
+                        const newBook = {
+                            cover: bookCover,
+                            title: book.title,
+                            year: book.first_publish_year,
+                            author: book.author_name ? book.author_name[0] : 'Unknown author',
+                            place: book.publish_place,
+                            editions: book.edition_key,
+                        }
+                        bookCache[index] = newBook;
+                        setExpanded(panel);
                     });
-                    setLoadingImg(false);
-                    setExpanded(panel);
                 });
-            }).catch(err => {
-                setBookDetails({
-                    title: book.title,
-                    author: book.author_name ? book.author_name[0] : 'Unknown author',
+            } else {
+                const newBook = {
                     cover: noCoverImg,
-                    url: book.edition_key.map((i) => `https://openlibrary.org/books/${i}`),
-                });
-                setLoadingImg(false);
+                    title: book.title,
+                    year: book.first_publish_year,
+                    author: book.author_name ? book.author_name[0] : 'Unknown author',
+                    place: book.publish_place,
+                    editions: book.edition_key,
+                }
+                bookCache[index] = newBook;
                 setExpanded(panel);
-            });
+            }
         }
     };
 
     useEffect(() => {
-        setBookDetails(null);
+        bookCache = {};
         setExpanded(false);
     }, [props.data]);
 
     return (
         <BookListContainer>
             {props.data.docs.map((book, i) =>
-                <Accordion key={i} expanded={expanded === `panel${i}`} onChange={handleChange(`panel${i}`, book)}>
+                <Accordion key={i} expanded={expanded === `panel${i}`} onChange={handleChange(`panel${i}`, book, i)}>
                     <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                         <Typography className={classes.heading}>{book.title}</Typography>
                         <Typography className={classes.secondaryHeading}>{book.author_name ? book.author_name[0] : ''}</Typography>
                     </AccordionSummary>
 
-                    {bookDetails && 
-                    <AccordionDetails>
+                    {bookCache[i] && expanded && <AccordionDetails>
                         <div className='bookContent'>
                             <div className='bookCover'>
-                                {!loadingImg && <img src={bookDetails.cover} alt={bookDetails.title}/>}
+                                <img src={bookCache[i].cover} alt={bookCache[i].title}/>
                             </div>
                             <div className='bookInfo'>
-                                <h1>{bookDetails.title}</h1>
-                                <p className={bookDetails.author === 'Unknown author' ? 'unknown' : ''}>
-                                    {bookDetails.author}
+                                <h1>{bookCache[i].title} {bookCache[i].year &&`(${bookCache[i].year})`}</h1>
+                                <h4 className={bookCache[i].author === 'Unknown author' ? 'italic' : ''}>
+                                    {bookCache[i].author}
+                                </h4>
+                                <p>{bookCache[i].place &&
+                                    bookCache[i].place.map((item, i) => {
+                                        return <span key={i}>{ (i ? ', ' : '') + item }</span>;
+                                    })}
                                 </p>
                             </div>
                         </div>
